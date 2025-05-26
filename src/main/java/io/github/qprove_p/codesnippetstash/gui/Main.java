06cd53126e9d4f513b25e3259495b01169ceb68d
qProve-P <pat.markvart@seznam.cz>
2025-05-26 22:51:19 +0200

    gitignore update, +logging in Main

--- src/main/java/io/github/qprove_p/codesnippetstash/gui/Main.java
+++ src/main/java/io/github/qprove_p/codesnippetstash/gui/Main.java
@@ -7,18 +7,30 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+
+import java.io.IOException;
 
 @Log4j2
 public class Main extends Application {
 
     public static void main(String[] args) {
+        log.info("Starting up");
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage){
+        log.info("Loading GUI");
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/appWindow.fxml"));
-        Parent root = loader.load();
+        Parent root = null;
+        try {
+            root = loader.load();
+            log.info("GUI loaded");
+        }catch(IOException e) {
+            log.error("Error loading GUI: ", e);
+            throw new RuntimeException(e);
+        }
 
         primaryStage.setTitle("Code Snippet Stash");
         primaryStage.setScene(new Scene(root));
