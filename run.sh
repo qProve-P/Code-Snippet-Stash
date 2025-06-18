e507d43f034c5b2bb73fb7366397b904c5c636f2
qProve-P <pat.markvart@seznam.cz>
2025-06-18 21:45:17 +0200

    run.sh update

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-java -jar target/code-snippet-stash-1.0-SNAPSHOT-shaded.jar
+JAVA_FX_PATH=/usr/share/javafx-21/javafx-sdk-21/lib
+
+java --module-path "$JAVA_FX_PATH" --add-modules javafx.controls,javafx.fxml \
+    -jar target/code-snippet-stash-1.0-SNAPSHOT-shaded.jar
